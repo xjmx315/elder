fcf01c0d95d1b18ca540ee5a0fbb18c95fb09d22
xjmx315 <xjmx315@gmail.com>
2025-03-02 16:37:14 +0900

    tester

--- tester/tester21/tester21.ino
+++ tester/tester21/tester21.ino
@@ -1,28 +1,92 @@
 /*
 엘더케어 테스트용 기기
 블루투스, 와이파이, OTA 테스트
+
+2x 버전은 블루투스 테스트를 위한 기능 업데이트 버전입니다. 
+앱과 ble 통신으로 데이터를 교환하고 연결 과정을 테스트합니다. 
+
+mpu: esp32-wroom-32D devkit
+mpu setting: ESP32 Dev Module
 */
+#define CONFIG_BT_NIMBLE_ENABLED 1
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <string.h>
 
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
 #include "WiFi.h"
 
-#define VERSION 10
+#define VERSION 21
 
-//핀 연결
+//***정의***
+//전역에서 쓰이는 일반적인 기능을 정의합니다. 
+
+//pin connect***************************************************************
 #define JOY_H 34
 #define JOY_V 35
 #define JOY_B 32
 
+#define LCD_SDA 21
+#define LCD_SCL 22
+
 #define LCD_W 128
 #define LCD_H 32
 
-//lcd
+//BLE***************************************************************
+#define SERVICE_UUID "180C"
+#define CHARACTERISTIC_UUID "2A57"
+
+
+BLEServer* pServer = nullptr;
+BLECharacteristic* pCharacteristic = nullptr;
+BLEService *pService = nullptr;
+bool deviceConnected = false;
+String receivedData = "";
+
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) override {
+        deviceConnected = true;
+        Serial.println("connect!");
+    }
+
+    void onDisconnect(BLEServer* pServer) override {
+        deviceConnected = false;
+        Serial.println("disconnect!");
+        pServer->getAdvertising()->start();
+    }
+};
+
+class MyCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic* pCharacteristic) override {
+        receivedData = pCharacteristic->getValue();
+        if (receivedData.length() > 0) {
+          Serial.print("수신된 데이터: ");
+          Serial.println(receivedData.c_str());
+
+          // 데이터 확인 후 응답 전송 (예: "OK" 응답)
+          String response = "ESP_ACK: " + receivedData;
+          pCharacteristic->setValue(response);
+          pCharacteristic->notify(); // 스마트폰으로 응답 전송
+        }
+    }
+};
+
+
+//lcd***************************************************************
 Adafruit_SSD1306 display(LCD_W, LCD_H, &Wire, -1);
 
-//정의
+void showmsg(auto msg){
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print(msg);
+  display.display();
+}
+//input event***************************************************************
+
 typedef void (*actionfuntion)();
 typedef bool (*eventfuntion)();
 
@@ -134,6 +198,7 @@ class EventManager{
   }
 };
 
+//options***************************************************************
 class Options{
   private:
   int num;
@@ -162,17 +227,28 @@ class Options{
   int getNum(){
     return num;
   }
+};
+
+char** getHomeMenu(int& n){
+  n = 4;
+  static char* testStrings[4];
+  testStrings[0] = "Joy Test";
+  testStrings[1] = "Wifi Test";
+  testStrings[2] = "BLE Test";
+  testStrings[3] = "OTA Test";
+
+  return testStrings;
 }
 
-
 //전역
-
 EventManager eventManager();
 
 int lastState = 0;
 int state = 0;
 //0: home
 //1: joyTest
+//2: wifiTest
+//3: BLETest
 
 void joyTest() {
   int joyX = analogRead(JOY_H);
@@ -191,8 +267,8 @@ void joyTest() {
 }
 
 int select(char** options, int optionNum) {
-  static int location = 0;
-  static int selected = 0;
+  static int location = 0; //화면에 그리는 4개의 옵션중 가장 윗 번호
+  static int selected = 0; //현재 선택중인 번호
   while (true){
     location = location%optionNum;
 
@@ -210,18 +286,26 @@ int select(char** options, int optionNum) {
 
     if (isUp()){
       selected--;
-      selected = selected%optionNum;
       if(selected < location){
         location = selected;
       }
+      if (selected < 0) {
+        selected = optionNum-1;
+        location = selected-3;
+      };
     }
+
     if (isDown()){
       selected++;
-      selected = selected%optionNum;
       if(selected > location + 3){
         location = selected;
       }
+      if (selected >= optionNum){
+        selected = 0;
+        location = 0;
+      }
     }
+
     if (isBtn()){
       return selected;
     }
@@ -231,13 +315,12 @@ int select(char** options, int optionNum) {
   }
 }
 
-
 void home(){
   static int location = 0;
   lastState = 0;
   int num, userSelection;
-  userSelection = select(getHomeMenu(&num), num);
-  state = userSelection+1
+  userSelection = select(getHomeMenu(num), num);
+  state = userSelection+1;
 }
 
 void setup() {
@@ -253,7 +336,9 @@ void setup() {
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
-  display.println("Tester 1.0");
+  display.print("Tester");
+  display.println(VERSION);
+
   display.display();
 
   WiFi.mode(WIFI_STA);
@@ -280,24 +365,13 @@ char** getTestString(int& n){
   return testStrings;
 }
 
-char** getHomeMenu(int& n){
-  n = 4;
-  static const char* testStrings[4];
-  testStrings[0] = "Joy Test";
-  testStrings[1] = "Wifi Test";
-  testStrings[2] = "BLE Test";
-  testStrings[3] = "OTA Test";
-
-  return testStrings;
-}
-
 char** getWifiSSID(int& n){
   /* wifi ssid 리스트를 검색해서 리턴합니다. 
   cstring 형태로 리턴하며 동적 할당되기 때문에 free해줘야 합니다. 
   n에 찾은 ssid의 갯수가 담깁니다. 
   */
   n = WiFi.scanNetworks();
-  static const char* nowifi = "No wifi";
+  static char* nowifi = "No wifi";
 
   static char* ssids[100];
   char* tmp;
@@ -316,6 +390,68 @@ char** getWifiSSID(int& n){
   return ssids;
 }
 
+void wifiTest() {
+  static char** ssids;
+  static int n, userInput;
+  showmsg("Wifi Reading...");
+  ssids = getWifiSSID(n);
+  userInput = select(ssids, n);
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print(userInput);
+  display.display();
+  delay(1000);
+
+  state = 0; //home으로 return
+}
+
+void initBLE(){
+  BLEDevice::init("ESP32_BLE");
+
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  pService = pServer->createService(SERVICE_UUID);
+
+  pCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+}
+
+void BLETest(){
+  showmsg("Initializing...");
+  static int isInitialized = 0;
+
+  if (isInitialized == 0){
+    initBLE();
+    isInitialized = 1;
+  }
+
+  pCharacteristic->setValue("Hello BLE");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(6);
+  pServer->getAdvertising()->start();
+
+  while (true){
+    if (deviceConnected) {
+      String message = String("ESP32 Y: ") + String(analogRead(JOY_V));
+      pCharacteristic->setValue(message);
+      pCharacteristic->notify();
+      showmsg(analogRead(JOY_V));
+      delay(100); //cpu양보
+    }
+    else{
+      showmsg("Advertising...");
+      vTaskDelay(700);
+    }
+  }
+}
+
 void loop() {
   switch (state){
     case 0: 
@@ -326,6 +462,9 @@ void loop() {
       break;
     case 2:
       wifiTest();
+      break;
+    case 3:
+      BLETest();
       break;
     default:
       state = 0;
